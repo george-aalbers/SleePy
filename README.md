e4174df0624e3b9febe6387b91276690acaa6b8f
George Aalbers <haalbers@violet.uvt.nl>
2022-01-25 15:48:55 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# neonsleep
+# NeonSleep
+
+This Github repository contains code for the project "NeonSleep: An open-source package for estimating sleep duration from smartphone log data". 
--- preprocessing.py
+++ preprocessing.py
@@ -5,94 +5,6 @@ print("Starting preprocessing for sleep estimation study.")
 import pandas as pd
 import numpy as np
 
-# Functions for preprocessing of the data
-
-# Imports
-import pandas as pd
-import numpy as np
-
-def scale_features():    
-    X = pd.read_csv("X.csv", index_col = 0)
-    X = X/900
-    X.to_csv("X.csv")
-
-def order_features():
-    X = pd.read_csv("X.csv", index_col = 0)
-    X = X[X.columns.sort_values(ascending=False).tolist()]
-    yesterday = X.columns[:12].tolist()
-    today = X.columns[12:].tolist()
-    today.extend(yesterday)
-    X = X[today]
-    X.to_csv("X.csv")
-    
-def transform_to_dataframe(df):
-    return pd.DataFrame(df)
-    
-def select_features_targets_ids(study_parameters):
-    
-    # Read preprocessed data
-    data = pd.read_csv("data.csv", index_col = 0, low_memory = False)
-    
-    # Split into X and y
-    ids = data[study_parameters["id_variable"]]
-    X = data[study_parameters["features"]]
-    y = data[study_parameters["targets"]]
-    
-    # Set X and y index to ids
-    X.index = ids
-    y.index = ids
-    
-    # Write to file
-    ids.to_csv("ids.csv")
-    X.to_csv("X.csv")
-    y.to_csv("y.csv")
-    
-def center_all_data_single_subject(df, study_parameters):
-    
-    # Center all data for one participant
-    df.iloc[:,1:] = df.iloc[:,1:] - df.iloc[:,1:].mean()
-    
-    return df
-    
-def within_person_center_nomothetic(train, test, study_parameters):
-    
-    # Transform data to dataframe
-    train = transform_to_dataframe(train).reset_index()
-    test = transform_to_dataframe(test).reset_index()
-    
-    # Center all data for multiple participants
-    train = train.groupby("id").apply(center_all_data_single_subject, study_parameters)
-    test = test.groupby("id").apply(center_all_data_single_subject, study_parameters)
-    
-    # Set index to id variable
-    train.set_index("id", inplace=True)
-    test.set_index("id", inplace=True)
-    
-    return train, test
-    
-def within_person_center_idiographic(train, test, study_parameters):
-    
-    # Center one person's data based on mean in train data
-    
-    # Get mean in train data
-    mean = train.mean()
-    
-    # Subtract from train data
-    train = train - mean
-    
-    # And subtract from test data
-    test = test - mean
-    
-    return train, test
-        
-def center(train, test, study_parameters):
-    
-    # Center data for each person, method depends on experiment type
-    if study_parameters["experiment_type"] == "idiographic":
-        return within_person_center_idiographic(train, test, study_parameters)
-    elif study_parameters["experiment_type"] == "nomothetic":
-        return within_person_center_nomothetic(train, test, study_parameters)
-
 def flatten_targets(df):
     return np.ravel(df)
 
@@ -178,10 +90,46 @@ def preprocess():
     data = data[data.iloc[:,3:].sum(axis=1) != 0]
     print("After dropping faulty features:", data.shape)
     
-    # Drop participants with < 50 observations
-    pps = data.id.value_counts()[data.id.value_counts() > 49]
+    # Drop participants with < 20 observations
+    pps = data.id.value_counts()[data.id.value_counts() > 19]
     data = data[data.id.isin(pps.index.tolist())]
     data.reset_index(inplace=True,drop=True)
     data.to_csv("data.csv")
 
     print("Finished data preprocessing for sleep estimation study.")
+    
+def scale_features():    
+    X = pd.read_csv("X.csv", index_col = 0)
+    X = X/900
+    X.to_csv("X.csv")
+
+def order_features():
+    X = pd.read_csv("X.csv", index_col = 0)
+    X = X[X.columns.sort_values(ascending=False).tolist()]
+    yesterday = X.columns[:12].tolist()
+    today = X.columns[12:].tolist()
+    today.extend(yesterday)
+    X = X[today]
+    X.to_csv("X.csv")
+    
+def transform_to_dataframe(df):
+    return pd.DataFrame(df)
+    
+def select_features_targets_ids(study_parameters):
+    
+    # Read preprocessed data
+    data = pd.read_csv("data.csv", index_col = 0, low_memory = False)
+    
+    # Split into X and y
+    ids = data[study_parameters["id_variable"]]
+    X = data[study_parameters["features"]]
+    y = data[study_parameters["targets"]]
+    
+    # Set X and y index to ids
+    X.index = ids
+    y.index = ids
+    
+    # Write to file
+    ids.to_csv("ids.csv")
+    X.to_csv("X.csv")
+    y.to_csv("y.csv")
